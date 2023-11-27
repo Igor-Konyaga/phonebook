06929b9d3ded8087032d29488ac738b361e18fdc
Igor-Konyaga <99217226+Igor-Konyaga@users.noreply.github.com>
2023-11-27 21:19:08 +0000

    Deploying to gh-pages from @ Igor-Konyaga/goit-react-hw-08-phonebook@b1bc81222f55ef18e45723cae477b1baa9c09972 🚀

---
+++ static/js/964.c775f66c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[964],{1964:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=964.c775f66c.chunk.js.map
