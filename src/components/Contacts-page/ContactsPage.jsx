9d31515e15b91e2cea9e1f6625da582103828cdc
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-11-28 13:11:23 +0200

    update

--- src/components/Contacts-page/ContactsPage.jsx
+++ src/components/Contacts-page/ContactsPage.jsx
@@ -1,14 +1,15 @@
 import { ContactForm } from './Contact-form/ContactForm';
 import { ContactList } from './Contact-list/ContactList';
 import { Filter } from './Filter/Filter';
+import css from './ContactsPage.module.css';
 
 const ContactsPage = () => {
   return (
-    <div>
+    <section className={css.section}>
       <ContactForm />
       <Filter />
       <ContactList />
-    </div>
+    </section>
   );
 };
 
